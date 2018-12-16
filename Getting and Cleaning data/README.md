ef0959d3f964cb7651ee829f845914fe02513224
Oscar017 <osmium008@gmail.com>
2018-12-16 22:23:40 +0800

    added Assignment

---
+++ Getting and Cleaning data/README.md
@@ -0,0 +1,14 @@
+### Peer-graded Assignment: Getting and Cleaning DAta Course Project
+
+##Data set
+[Human Activity Recognition Using Smartphones](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+
+##Files
+1. Codebook.md is a codebook that describes the data, variables and captures any changes.
+2. final_data.txt is an exported file afer cleaning up the data
+3. run_analysis.R is a script that shows the execution of getting and cleaning the data as described in the course requirements:
+  + Merges the training and test sets to create one data set.
+  + Extracts only the measurement on the mean and standard deviation for each measurement.
+  + USes descriptive activity names to name the activities in the data set.
+  + Appropriately labels the data set with descriptive variable names.
+  +From the data in step4, creates a second independent tidy data set with the average of each variable for each activity and each subject.
--- Getting and Cleaning data/run_analysis.R
+++ Getting and Cleaning data/run_analysis.R
@@ -1,1 +1,59 @@
+library(dplyr)
+library(magrittr)
 
+filename <- "Coursera_DS3_Final.zip"
+
+# Checking if archieve already exists.
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename, method="curl")
+}  
+
+# Checking if folder exists
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+
+
+Y <- rbind(y_train, y_test)
+X <- rbind(x_train, x_test)
+Subject <- rbind(subject_train, subject_test)
+merged_data <- cbind(Subject, Y, X)
+
+tidy_data <- merged_data %>% select(subject, code, contains("mean"), contains("std"))
+tidy_data$code <- activities[tidy_data$code, 2]
+
+names(tidy_data)[2] = "activity"
+names(tidy_data)<-gsub("Acc", "Accelerometer", names(tidy_data))
+
+names(tidy_data)<-gsub("Mag", "Magnitude", names(tidy_data))
+names(tidy_data)<-gsub("^t", "Time", names(tidy_data))
+names(tidy_data)<-gsub("^f", "Frequency", names(tidy_data))
+names(tidy_data)<-gsub("Gyro", "Gyroscope", names(tidy_data))
+names(tidy_data)<-gsub("-freq()", "Frequency", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("angle", "Angle", names(tidy_data))
+names(tidy_data)<-gsub("gravity", "Gravity", names(tidy_data))
+names(tidy_data)<-gsub("BodyBody", "Body", names(tidy_data))
+names(tidy_data)<-gsub("-std()", "STD", names(tidy_data), ignore.case = TRUE)
+names(tidy_data)<-gsub("tBody", "TimeBody", names(tidy_data))
+names(tidy_data)<-gsub("-mean()", "Mean", names(tidy_data), ignore.case = TRUE)
+
+final_data <- tidy_data %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(final_data, "final_data.txt", row.name=FALSE)
+
+str(final_data)
+
+final_data
